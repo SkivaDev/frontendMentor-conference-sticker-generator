a4e6c8fb10512ea88d234e2c9c9c8865326837a8
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2025-01-08 09:29:19 -0500

    feat(project): :lipstick: add Tailwind config styles, and globals.css for challenge

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,13 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        neutral: "var(--neutral)",
+        'neutral-300': "var(--neutral-300)",
+        'neutral-500': "var(--neutral-500)",
+        'neutral-700': "var(--neutral-700)",
+        'neutral-900': "var(--neutral-900)",
+        'orange-500': "var(--orange-500)",
+        'orange-700': "var(--orange-700)",
       },
     },
   },
