73ae27ce3e9c3f212c2712528f07587915c37864
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2025-02-24 19:04:14 -0500

    style(Header): :lipstick: add mobile responsive design

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,8 +4,8 @@ import React from 'react'
 const Header = () => {
   return (
     <header className='w-full'>
-        <div className='w-full h-[110px] flex justify-center items-center'>
-            <Image alt='logo full' src={'/images/logo-full.svg'} width={210} height={210}/>
+        <div className='w-full h-[90px] md:h-[110px] flex justify-center items-center'>
+            <Image alt='logo full' src={'/images/logo-full.svg'} width={210} height={210} className='w-[168px] h-auto md:w-[210px] md:h-[210px]'/>
         </div>
     </header>
   )
